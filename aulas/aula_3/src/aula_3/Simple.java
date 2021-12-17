27894705431ba1888d8e6bbef494906b9e5782a0
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2021-12-16 21:38:08 -0300

    arquivos criados na aula 3 - criação de classe e métodos e variáveis estáticas

---
+++ aulas/aula_3/src/aula_3/Simple.java
@@ -0,0 +1,10 @@
+package aula_3;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		System.out.print("Hello, Java!");
+
+	}
+
+}
---
+++ aulas/aula_3/src/terminal/TerminalBanco.java
@@ -0,0 +1,19 @@
+package terminal;
+
+import java.util.Scanner;
+
+public class TerminalBanco {
+
+	
+	
+	public static void main(String[] args) {
+		System.out.println(somar(2,3));
+	}
+	
+	
+	public static int somar(int n1, int n2) {
+		return n1 + n2;
+	}
+	
+	
+}
