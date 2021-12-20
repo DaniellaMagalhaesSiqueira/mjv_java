db229d6ebce12dbbe0c6a0c42e86b67e71e1a9a7
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2021-12-20 20:32:09 -0300

    criando terminal

---
+++ aulas/aula_5/src/banckapp/model/Conta.java
@@ -0,0 +1,47 @@
+package banckapp.model;
+//poo
+//modificadores de acesso
+//padrão java beans
+//depuração da ferramenta eclipse
+//atalhos para produtividade de código [menu source -> Generate getters and setters]
+// métodos getters -> getN + ctrl + space autocompleta
+public class Conta {
+//2 a 3 atributos
+	private double saldo;
+	private int numero;
+	private double chequeEspecial;
+	private String tipo;
+	
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public int getNumero() {
+		return numero;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public double getChequeEspecial() {
+		return chequeEspecial;
+	}
+	public void setChequeEspecial(double chequeEspecial) {
+		this.chequeEspecial = chequeEspecial;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+	
+	
+	
+}
---
+++ aulas/aula_5/src/banckapp/service/ContaService.java
@@ -0,0 +1,41 @@
+package banckapp.service;
+
+import banckapp.model.Conta;
+
+public class ContaService {
+//2 a 3 recursos
+	public void depositar(Conta conta, double valorDepositado) {
+		conta.setSaldo(valorDepositado);
+	}
+	public void sacar(Conta conta, double valorSacado) {
+		if(valorSacado > conta.getSaldo() + conta.getChequeEspecial()) {
+			System.out.println("Saldo insuficiente para realizar a operação");
+			return;
+		}else {
+			if(conta.getSaldo() < valorSacado) {
+				conta.setSaldo(0.0);
+				conta.setChequeEspecial(conta.getSaldo() + conta.getChequeEspecial() - valorSacado);
+			}else {
+				
+				conta.setSaldo(conta.getSaldo() - valorSacado);
+			}
+		}
+	}
+	
+	public void transferir(Conta depositante, Conta depositada, double valorTransferido) {
+		if(depositante.getSaldo() + depositante.getChequeEspecial() < valorTransferido) {
+			System.out.println("Saldo insuficiente para realizar a operação");
+			return;
+		}else {
+			if(depositante.getSaldo() < valorTransferido) {
+				depositante.setSaldo(0.0);
+				depositante.setChequeEspecial(depositante.getSaldo() + depositante.getChequeEspecial() - valorTransferido);
+				this.depositar(depositada, valorTransferido);
+			}else {
+				depositante.setSaldo(depositante.getSaldo() - valorTransferido);
+				this.depositar(depositada, valorTransferido);
+			}
+		}
+		
+	}
+}
